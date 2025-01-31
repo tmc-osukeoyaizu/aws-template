c6750c01a5e00386eba0c937d06c4be821082261
tmc-osukeoyaizu <osuke_oyaizu@mail.toyota.co.jp>
2025-01-31 10:30:40 +0900

    Update scripts.sh

--- CloudQuest/Serverless/Amazon Q Developerでアプリをより迅速に構築/scripts.sh
+++ CloudQuest/Serverless/Amazon Q Developerでアプリをより迅速に構築/scripts.sh
@@ -4,3 +4,6 @@ zip -r addObjects.zip addObjects.py test.txt
 aws iam list-roles --query 'Roles[?contains(RoleName, `LambdaDeploymentRole`) == `true`].Arn' --output text
 # aws cli command to upload a lambda function code.zip file with addObjects.lambda_handler and runtime 3.12
 aws lambda create-function --function-name addObjects --zip-file fileb://addObjects.zip --handler addObjects.lambda_handler --runtime python3.12 --role <arn>
+
+
+Lambda関数のタイムアウト時間を変更する
